a5e523b4fb895dd0dfcf3d639655500734ceafb9
Denis <ad.way@mail.ri>
2024-07-25 14:04:17 +0400

    step 1.1

--- apps/books/views.py
+++ apps/books/views.py
@@ -7,20 +7,14 @@ from django.contrib import messages
 from django.utils import timezone
 from .models import Book
 from .forms import BookCreationForm
+from core.mixins import BorrowListMixin, UserIsLibrarianRequiredMixin
 
 
-class BookCreationView(CreateView):
+class BookCreationView(UserIsLibrarianRequiredMixin, CreateView):
     model = Book
     form_class = BookCreationForm
     template_name = 'books/create.html'
     success_url = reverse_lazy('list')
-
-    def test_func(self):
-        return self.request.user.is_librarian
-
-    def handle_no_permission(self):
-        messages.error(self.request, 'У вас нет прав для добавления книг.')
-        return redirect('list')
 
 
 class BookListView(ListView):
@@ -30,26 +24,11 @@ class BookListView(ListView):
     ordering = ['title']
 
 
-class BorrowListUsersView(ListView):
+class BorrowListUsersView(BorrowListMixin, ListView):
     model = Book
     template_name = 'books/list_borrow.html'
     context_object_name = 'list_borrow'
     ordering = ['borrowed_by']
-
-    def get_queryset(self):
-        return Book.objects.filter(is_borrowed=True).select_related('borrowed_by')
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        current_time = timezone.now()
-        for book in context['list_borrow']:
-            book.borrowed_duration = current_time - book.borrowed_date
-
-            days = book.borrowed_duration.days
-            hours, remainder = divmod(book.borrowed_duration.seconds, 3600)
-            minutes, seconds = divmod(remainder, 60)
-            book.borrowed_duration = '{:d} дн {:02} ч {:02} мин'.format(days, hours, minutes)
-        return context
 
 
 class MyBooksView(TemplateView):
@@ -72,8 +51,6 @@ class BorrowBookView(View):
                 book.borrowed_date = None
                 messages.info(request, f'Вы успешно вернули книгу "{book.title}".')
 
-            else:
-                messages.error(request, f'Книга "{book.title}" уже взята другим пользователем.', extra_tags='danger')
 
         else:
             book.is_borrowed = True
--- core/mixins.py
+++ core/mixins.py
@@ -1,5 +1,9 @@
 from django.contrib.auth import login
+from django.utils import timezone
+from django.contrib import messages
+from apps.books.models import Book
 from apps.users.forms import ReaderRegisterForm
+from django.shortcuts import redirect
 
 
 class UserFormMixin:
@@ -25,3 +29,29 @@ class UserFormMixin:
             return response
         else:
             return self.render_to_response(self.get_context_data(form=form))
+
+
+class BorrowListMixin:
+    def get_queryset(self):
+        return Book.objects.filter(is_borrowed=True).select_related('borrowed_by')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        current_time = timezone.now()
+        for book in context['list_borrow']:
+            book.borrowed_duration = current_time - book.borrowed_date
+
+            days = book.borrowed_duration.days
+            hours, remainder = divmod(book.borrowed_duration.seconds, 3600)
+            minutes, seconds = divmod(remainder, 60)
+            book.borrowed_duration = '{:d} дн {:02} ч {:02} мин'.format(days, hours, minutes)
+        return context
+
+
+class UserIsLibrarianRequiredMixin:
+    def test_func(self):
+        return self.request.user.is_librarian
+
+    def handle_no_permission(self):
+        messages.error(self.request, 'У вас нет прав для добавления книг.')
+        return redirect('list')
